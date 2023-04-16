f9c6e5443e3c8d0ac3bbbc73f11807fdc03399a8
Armen <armarmsat@gmail.com>
2023-04-16 23:18:26 +0400

    update

---
+++ utils/classes.py
@@ -0,0 +1,48 @@
+from utils.database import Database
+
+class Note():
+    def __init__(self,data:dict,db:Database) -> None:
+        self.data = data
+        self.owner:str = data['owner']
+        self.id:str = data['id']
+        self.note_text:str = data['note_text']
+        self.users:list = data['team']
+        self.users = []
+        self.settings:dict = data['settigns']
+        self.auth_code:str = data['auth_code']
+    def back_to_json(self):
+        return self.data
+class PartialUser():
+    def __init__(self,data:dict,db:Database) -> None:
+        self.data = data 
+        self.uid:str = data['uid']
+        self.username:str = data['username']
+
+    def back_to_json(self):
+        return self.data
+class AuthenticatedUser():
+    def __init__(self,data:dict,db:Database) -> None:
+        self.data = data
+        self.uid:str = data['uid']
+        self.email:str = data['email']
+        self.password_hash:str = data['password_hash']
+        self.token:str = data['token']
+        self.username:str = data['username']
+        self.display_name:str = data['username']
+        
+    def back_to_json(self):
+        return self.data
+class UserSettings():
+    def __init__(self,data:dict) -> None:
+        self.data = data
+        self.enable_invites:bool = data['invite_privacy']
+        
+    def back_to_json(self):
+        return self.data
+class NoteSettngs():
+    def __init__(self,data:dict,db:Database) -> None:
+        self.data = data
+        self.auto_delete:int = data['auto_delete']
+        
+    def back_to_json(self):
+        return self.data
